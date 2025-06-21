353a16523ff6825a7c56facb432e071d6f59828d
Hoang Nguyen <rrgyfdeds@gmail.com>
2025-06-21 18:02:05 +0700

    Added My Purchased Courses section

--- Controllers/LearnerController.cs
+++ Controllers/LearnerController.cs
@@ -1,10 +1,25 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using JapaneseLearningPlatform.Models;
+using JapaneseLearningPlatform.Data;
 
 namespace NihongoSekaiPlatform.Controllers
 {
     public class LearnerController : Controller
     {
+        private readonly AppDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public LearnerController(AppDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
         // GET: LearnerController
         public ActionResult Index()
         {
@@ -79,5 +94,35 @@ namespace NihongoSekaiPlatform.Controllers
                 return View();
             }
         }
+
+        [Authorize(Roles = "Learner")]
+        public async Task<IActionResult> MyPurchasedCourses(int page = 1, int pageSize = 6)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var purchasedCourseIds = await _context.Orders
+                .Where(o => o.UserId == userId)
+                .SelectMany(o => o.OrderItems.Select(oi => oi.CourseId))
+                .Distinct()
+                .ToListAsync();
+
+            var allPurchasedCourses = await _context.Courses
+                .Where(c => purchasedCourseIds.Contains(c.Id))
+                .ToListAsync();
+
+            var totalItems = allPurchasedCourses.Count;
+            var totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
+
+            var pagedCourses = allPurchasedCourses
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            ViewBag.TotalPages = totalPages;
+            ViewBag.CurrentPage = page;
+
+            return View("MyPurchasedCourses", pagedCourses);
+        }
+
     }
 }
