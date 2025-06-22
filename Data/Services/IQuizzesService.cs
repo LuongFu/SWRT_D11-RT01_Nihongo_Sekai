eebd7e6ef7ca9910bfb5c058bb1e3786dbf088be
Luong Fu <lmp14589@gmail.com>
2025-06-22 18:10:15 +0700

    Add CRUD feature section and content item for a better course details (#30)

---
+++ Data/Services/IQuizzesService.cs
@@ -0,0 +1,17 @@
+﻿using JapaneseLearningPlatform.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JapaneseLearningPlatform.Data.Services
+{
+
+    public interface IQuizzesService
+    {
+        Task<Quiz> GetQuizByIdAsync(int id);
+        Task<IEnumerable<Quiz>> GetAllAsync();
+        Task AddAsync(Quiz quiz);
+        Task UpdateAsync(Quiz quiz);
+        Task DeleteAsync(int id);
+    }
+
+}
