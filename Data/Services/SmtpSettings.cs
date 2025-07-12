352d661df81fbdb0b1e18d3a4299104daf48eb34
Luong Fu <lmp14589@gmail.com>
2025-07-12 21:22:18 +0700

    fix enable ssl for email send destination

--- Data/Services/SmtpSettings.cs
+++ Data/Services/SmtpSettings.cs
@@ -7,7 +7,7 @@ public class SmtpSettings
 {
     public string Host { get; set; } = "";
     public int Port { get; set; }
-    public bool EnableSsl { get; set; }
+    public bool EnableSsl { get; set; } = true;
     public string UserName { get; set; } = "";
     public string Password { get; set; } = "";
     public string From { get; set; } = "";
